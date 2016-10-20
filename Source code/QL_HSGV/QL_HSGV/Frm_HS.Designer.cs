b17e8df83a3e7e91c037f1949ed9adc1578d81fe
Shindo <bachngochieph2@gmail.com>
2016-10-20 15:19:08 +0700

    module tìm tiếm học sinh
    hoàn thành module tìm kiếm học sinh

--- Source code/QL_HSGV/QL_HSGV/Frm_HS.Designer.cs
+++ Source code/QL_HSGV/QL_HSGV/Frm_HS.Designer.cs
@@ -28,35 +28,269 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.DTGV = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.DTGV)).BeginInit();
+            this.dgvHocSinh = new System.Windows.Forms.DataGridView();
+            this.clmMaHS = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmHoten = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmNgaySinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmGT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmDiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmQue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmLớp = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtTimMa = new System.Windows.Forms.TextBox();
+            this.txtTimTen = new System.Windows.Forms.TextBox();
+            this.txtTimLop = new System.Windows.Forms.TextBox();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnLammoi = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHocSinh)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // DTGV
+            // dgvHocSinh
             // 
-            this.DTGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DTGV.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.DTGV.Location = new System.Drawing.Point(0, 0);
-            this.DTGV.Name = "DTGV";
-            this.DTGV.Size = new System.Drawing.Size(981, 408);
-            this.DTGV.TabIndex = 0;
+            this.dgvHocSinh.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHocSinh.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmMaHS,
+            this.clmHoten,
+            this.clmNgaySinh,
+            this.clmGT,
+            this.clmDiaChi,
+            this.clmQue,
+            this.clmLớp});
+            this.dgvHocSinh.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvHocSinh.Location = new System.Drawing.Point(0, 0);
+            this.dgvHocSinh.Name = "dgvHocSinh";
+            this.dgvHocSinh.Size = new System.Drawing.Size(981, 352);
+            this.dgvHocSinh.TabIndex = 0;
+            this.dgvHocSinh.SelectionChanged += new System.EventHandler(this.DTGV_SelectionChanged);
+            // 
+            // clmMaHS
+            // 
+            this.clmMaHS.DataPropertyName = "maHS";
+            this.clmMaHS.Frozen = true;
+            this.clmMaHS.HeaderText = "Mã Học Sinh";
+            this.clmMaHS.Name = "clmMaHS";
+            // 
+            // clmHoten
+            // 
+            this.clmHoten.DataPropertyName = "hovaten";
+            this.clmHoten.Frozen = true;
+            this.clmHoten.HeaderText = "Họ Và Tên";
+            this.clmHoten.Name = "clmHoten";
+            this.clmHoten.Width = 200;
+            // 
+            // clmNgaySinh
+            // 
+            this.clmNgaySinh.DataPropertyName = "ngaysinh";
+            this.clmNgaySinh.HeaderText = "Ngày Sinh";
+            this.clmNgaySinh.Name = "clmNgaySinh";
+            this.clmNgaySinh.Width = 120;
+            // 
+            // clmGT
+            // 
+            this.clmGT.DataPropertyName = "GT";
+            this.clmGT.HeaderText = "Giới Tính";
+            this.clmGT.Name = "clmGT";
+            // 
+            // clmDiaChi
+            // 
+            this.clmDiaChi.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmDiaChi.DataPropertyName = "diachi";
+            this.clmDiaChi.HeaderText = "Địa Chỉ";
+            this.clmDiaChi.Name = "clmDiaChi";
+            // 
+            // clmQue
+            // 
+            this.clmQue.DataPropertyName = "quequan";
+            this.clmQue.HeaderText = "Quê Quán";
+            this.clmQue.Name = "clmQue";
+            this.clmQue.Width = 150;
+            // 
+            // clmLớp
+            // 
+            this.clmLớp.DataPropertyName = "tenLop";
+            this.clmLớp.HeaderText = "Lớp";
+            this.clmLớp.Name = "clmLớp";
+            this.clmLớp.Width = 150;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(981, 112);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dgvHocSinh);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 112);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(981, 352);
+            this.panel2.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnXoa);
+            this.groupBox1.Controls.Add(this.btnLammoi);
+            this.groupBox1.Controls.Add(this.btnSua);
+            this.groupBox1.Controls.Add(this.btnThem);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(981, 67);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chức năng";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtTimLop);
+            this.groupBox2.Controls.Add(this.txtTimTen);
+            this.groupBox2.Controls.Add(this.txtTimMa);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(0, 67);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(981, 45);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tìm Kiếm";
+            // 
+            // txtTimMa
+            // 
+            this.txtTimMa.Location = new System.Drawing.Point(42, 22);
+            this.txtTimMa.Name = "txtTimMa";
+            this.txtTimMa.Size = new System.Drawing.Size(97, 20);
+            this.txtTimMa.TabIndex = 0;
+            this.txtTimMa.Text = "  -- Mã học sinh";
+            this.txtTimMa.Click += new System.EventHandler(this.txtTimMa_Click);
+            this.txtTimMa.TextChanged += new System.EventHandler(this.txtTimMa_TextChanged);
+            // 
+            // txtTimTen
+            // 
+            this.txtTimTen.Location = new System.Drawing.Point(143, 22);
+            this.txtTimTen.Name = "txtTimTen";
+            this.txtTimTen.Size = new System.Drawing.Size(197, 20);
+            this.txtTimTen.TabIndex = 0;
+            this.txtTimTen.Text = "   -- Họ tên học sinh";
+            this.txtTimTen.Click += new System.EventHandler(this.txtTimTen_Click);
+            this.txtTimTen.TextChanged += new System.EventHandler(this.txtTimTen_TextChanged);
+            // 
+            // txtTimLop
+            // 
+            this.txtTimLop.Location = new System.Drawing.Point(829, 19);
+            this.txtTimLop.Name = "txtTimLop";
+            this.txtTimLop.Size = new System.Drawing.Size(149, 20);
+            this.txtTimLop.TabIndex = 0;
+            this.txtTimLop.Text = "--Lớp";
+            this.txtTimLop.Click += new System.EventHandler(this.txtTimLop_Click);
+            this.txtTimLop.TextChanged += new System.EventHandler(this.txtTimLop_TextChanged);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnXoa.Enabled = false;
+            this.btnXoa.Image = global::QL_HSGV.Properties.Resources.Delete;
+            this.btnXoa.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnXoa.Location = new System.Drawing.Point(307, 19);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 42);
+            this.btnXoa.TabIndex = 0;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnXoa.UseVisualStyleBackColor = false;
+            // 
+            // btnLammoi
+            // 
+            this.btnLammoi.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnLammoi.Image = global::QL_HSGV.Properties.Resources.Button_Refresh_icon__1_;
+            this.btnLammoi.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLammoi.Location = new System.Drawing.Point(191, 19);
+            this.btnLammoi.Name = "btnLammoi";
+            this.btnLammoi.Size = new System.Drawing.Size(93, 42);
+            this.btnLammoi.TabIndex = 0;
+            this.btnLammoi.Text = "Làm mới";
+            this.btnLammoi.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnLammoi.UseVisualStyleBackColor = false;
+            // 
+            // btnSua
+            // 
+            this.btnSua.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnSua.Enabled = false;
+            this.btnSua.Image = global::QL_HSGV.Properties.Resources.edit_icon__1_;
+            this.btnSua.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSua.Location = new System.Drawing.Point(93, 19);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 42);
+            this.btnSua.TabIndex = 0;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSua.UseVisualStyleBackColor = false;
+            // 
+            // btnThem
+            // 
+            this.btnThem.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnThem.Image = global::QL_HSGV.Properties.Resources.Actions_list_add_icon__1_;
+            this.btnThem.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnThem.Location = new System.Drawing.Point(0, 19);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 42);
+            this.btnThem.TabIndex = 0;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnThem.UseVisualStyleBackColor = false;
             // 
             // Frm_HS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(981, 408);
-            this.Controls.Add(this.DTGV);
+            this.ClientSize = new System.Drawing.Size(981, 464);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "Frm_HS";
             this.Text = "Frm_HS";
             this.Load += new System.EventHandler(this.Frm_HS_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.DTGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHocSinh)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView DTGV;
+        private System.Windows.Forms.DataGridView dgvHocSinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmMaHS;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmHoten;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmNgaySinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmGT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmDiaChi;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmQue;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmLớp;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtTimLop;
+        private System.Windows.Forms.TextBox txtTimTen;
+        private System.Windows.Forms.TextBox txtTimMa;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnLammoi;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.Panel panel2;
     }
 }
--- Source code/QL_HSGV/QL_HSGV/Frm_HS.cs
+++ Source code/QL_HSGV/QL_HSGV/Frm_HS.cs
@@ -12,6 +12,9 @@ namespace QL_HSGV
 {
     public partial class Frm_HS : Form
     {
+        private bool _dangtimma = false;
+        private bool _dangtimten = false;
+        private bool _dangtimlop = false;
         ConnectDB CN = new ConnectDB();
         public Frm_HS()
         {
@@ -20,7 +23,92 @@ namespace QL_HSGV
 
         private void Frm_HS_Load(object sender, EventArgs e)
         {
-            DTGV.DataSource = CN.Get("Select * from tblHocSinh");
+            dgvHocSinh.DataSource = CN.Get("Select MaHS,Hovaten,GT,Ngaysinh ,Diachi,quequan,TenLop from tblhocsinh a,tblLop b where a.Malop=b.MaLop");
         }
+
+        private void DTGV_SelectionChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                dgvHocSinh.CurrentRow.Selected = true; //dữ liệu đc chọn cả dòng
+                btnSua.Enabled = true;
+                btnXoa.Enabled = true;
+            }
+            catch
+            { }
+        }
+        private void txtTimMa_TextChanged(object sender, EventArgs e)
+        {
+            _dangtimma = true;
+            string _sql = "";
+            if (_dangtimma == true)
+            {
+                _sql = @"Select MaHS,Hovaten,GT,Ngaysinh ,Diachi,quequan,TenLop from tblhocsinh a,tblLop b where a.Malop=b.MaLop and mahs like '%" + txtTimMa.Text + "%'";
+                if (_dangtimten)
+                {
+                    _sql += " and hovaten like N'%" + txtTimTen.Text + "%'";
+                }
+                if (_dangtimlop)
+                {
+                    _sql += " and tenlop = N'" + txtTimLop.Text + "'";
+                }
+
+                dgvHocSinh.DataSource = CN.Get(_sql);
+            }
+        }
+
+        private void txtTimTen_TextChanged(object sender, EventArgs e)
+        {
+              _dangtimten = true;
+            string _sql = "";
+            if (_dangtimten == true)
+            {
+                _sql = @"Select MaHS,Hovaten,GT,Ngaysinh ,Diachi,quequan,TenLop from tblhocsinh a,tblLop b where a.Malop=b.MaLop and hovaten like N'%" + txtTimTen.Text + "%'";
+                if (_dangtimma)
+                {
+                    _sql += " and mahs like N'%" + txtTimMa.Text + "%'";
+                }
+                if (_dangtimlop)
+                {
+                    _sql += " and tenlop = N'" + txtTimLop.Text + "'";
+                }
+                dgvHocSinh.DataSource = CN.Get(_sql);
+            }
+        }
+
+        private void txtTimLop_TextChanged(object sender, EventArgs e)
+        {
+            _dangtimlop = true;
+            string _sql = "";
+            if (_dangtimlop == true)
+            {
+                _sql = @"Select MaHS,Hovaten,GT,Ngaysinh ,Diachi,quequan,TenLop from tblhocsinh a,tblLop b where a.Malop=b.MaLop and tenlop like N'%" + txtTimLop.Text + "%'";
+                if (_dangtimma)
+                {
+                    _sql += " and mahs like N'%" + txtTimMa.Text + "%'";
+                }
+                if (_dangtimten)
+                {
+                    _sql += " and hovaten like N'%" + txtTimTen.Text + "%'";
+                }
+                dgvHocSinh.DataSource = CN.Get(_sql);
+            }
+        }
+
+        private void txtTimMa_Click(object sender, EventArgs e)
+        {
+            if (!_dangtimma) txtTimMa.Text = "";
+        }
+
+        private void txtTimTen_Click(object sender, EventArgs e)
+        {
+            if (!_dangtimten) txtTimTen.Text = "";
+        }
+
+        private void txtTimLop_Click(object sender, EventArgs e)
+        {
+            if (!_dangtimlop) txtTimLop.Text = "";
+        }
+
     }
 }
